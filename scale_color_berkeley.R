8315392d5e2351e60a7a1c6bb5180a93b4ec1704
Andrew McCartney <acm9q@virginia.edu>
2019-02-21 10:25:42 -0500

    Create scale_color_berkeley.R

---
+++ scale_color_berkeley.R
@@ -0,0 +1,18 @@
+berkeley <- c( 
+berkeleyblue = "#003262" ,
+founders = "#3B7EA1" ,
+caligold = "#FDB515" ,
+medalist = "#C4820E" ,
+wellman = "#D9661F" ,
+rosegarden = "#EE1F60" ,
+bridge = "#ED4E33" ,
+southhall = "#6C3302" ,
+bayfog = "#DDD5C7" ,
+lawrence = "#00B0DA" ,
+laplane = "#00A598" ,
+pacific = "#46535E" ,
+sather= "#B9D3B6" ,
+ion = "#CFDD45" ,
+soybean= "#859438" ,
+stone_pine = "#584F29"
+)
