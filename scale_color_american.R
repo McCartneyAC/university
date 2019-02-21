ccb24a4877d2df79b7538e749b4df7fbad3913f9
Andrew McCartney <acm9q@virginia.edu>
2019-02-21 09:29:26 -0500

    Create scale_color_american.R

---
+++ scale_color_american.R
@@ -0,0 +1,10 @@
+american<-c(
+altblue = "#13477D",
+altred = "#961e28",
+taupe = "#7E6D5F",
+slate = "#406B72",
+teal = "#67854e",
+teal = "#008290",
+yellow = "#eeb137",
+warm = "#005578",
+librarygrey = "#5e6769")
