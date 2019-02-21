4b09631d3dc3718f42b644cb7148f01ae7dbd1ad
Andrew McCartney <acm9q@virginia.edu>
2019-02-21 09:25:59 -0500

    Create scale_color_vatech.R

---
+++ scale_color_vatech.R
@@ -0,0 +1,13 @@
+vatech<-c(
+chicago = "#8B1F41",
+orange = "#E87722",
+stone = "#75787B",
+yardline = "#FFFFFF",
+pamplin = "#508590",
+triumphant = "#F7EA48",
+boundless = "#CE0058",
+Pylon = "#642667",
+Sunset = "#ED8B00",
+turquoise = "#2CD5C4",
+skipper = "#E5E1E6",
+landgrant = "#D7D2CB")
