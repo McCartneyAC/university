599c2f010ca0e8951d2ae1503144e06a998cc2bf
Andrew McCartney <acm9q@virginia.edu>
2019-02-21 11:13:38 -0500

    Create scale_color_nyu.R

---
+++ scale_color_nyu.R
@@ -0,0 +1,19 @@
+nyu <- c(
+"#57068c",
+"#000000",
+"#b8b8b8",
+"#cb0200",
+"#e86c00",
+"#489141",
+"#28619e", 
+"#3dbbdb", 
+"#007c70", 
+"#d71e5e", 
+"#e86c00", 
+"#ffc107"
+)
+
+nyu_continuous <- c(
+# these will need some modification as points within
+# scale_color_continuous to work
+"#8900e1","#57068c","#330662","#220337")
