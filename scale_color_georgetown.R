fa605c290009735595ebc923ee475f7c131f82a6
Andrew McCartney <acm9q@virginia.edu>
2019-02-21 11:25:56 -0500

    Create scale_color_georgetown.R

---
+++ scale_color_georgetown.R
@@ -0,0 +1,13 @@
+georgetown <- c(
+blue = "#041e42", 
+gray = "#63666a", 
+pantone280 = "#012169", 
+pantone293 = "#003da5", 
+coolgray4 = "#bbbcbc", 
+pantone7527 = "#d6d2c4", 
+pantone306 = "#00b5e2", 
+pantone369 = "#64a70b", 
+pantone199 = "#d50032", 
+pantone202 = "#862633", 
+pantone1205  = "#f8e08e"
+)
