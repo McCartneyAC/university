276d187921d4f8d12d3cba8573ee1a332a24447d
Andrew McCartney <acm9q@virginia.edu>
2019-03-05 21:20:06 -0500

    Update scale_color_yale.R

--- scale_color_yale.R
+++ scale_color_yale.R
@@ -10,3 +10,30 @@ yale <- c(
 "#5f712d",
 "#bd5319"
 )
+
+pal_yale <- function(palette = c("yale"), alpha = 1) {
+  palette <- match.arg(palette)
+  
+  if (alpha > 1L | alpha <= 0L) stop("alpha must be in (0, 1]")
+  
+  raw_cols <- yale
+  raw_cols_rgb <- col2rgb(raw_cols)
+  alpha_cols <- rgb(
+    raw_cols_rgb[1L, ], raw_cols_rgb[2L, ], raw_cols_rgb[3L, ],
+    alpha = alpha * 255L, names = names(raw_cols),
+    maxColorValue = 255L
+  )
+  
+  scales::manual_pal(unname(alpha_cols))
+}
+
+
+scale_color_yale <- function(palette = c("yale"), alpha = 1, ...) {
+  palette <- match.arg(palette)
+  discrete_scale("colour", "yale", pal_yale(palette, alpha), ...)
+}
+
+scale_fill_yale <- function(palette = c("yale"), alpha = 1, ...) {
+  palette <- match.arg(palette)
+  discrete_scale("fill", "yale", pal_yale(palette, alpha), ...)
+}
