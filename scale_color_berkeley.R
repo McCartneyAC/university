8611a5424a73d8e9691d74d2985b0502c013c750
Andrew McCartney <acm9q@virginia.edu>
2019-03-05 21:06:26 -0500

    Update scale_color_berkeley.R

--- scale_color_berkeley.R
+++ scale_color_berkeley.R
@@ -16,3 +16,30 @@ ion = "#CFDD45" ,
 soybean= "#859438" ,
 stone_pine = "#584F29"
 )
+
+pal_berkeley <- function(palette = c("berkeley"), alpha = 1) {
+  palette <- match.arg(palette)
+  
+  if (alpha > 1L | alpha <= 0L) stop("alpha must be in (0, 1]")
+  
+  raw_cols <- uvapal
+  raw_cols_rgb <- col2rgb(raw_cols)
+  alpha_cols <- rgb(
+    raw_cols_rgb[1L, ], raw_cols_rgb[2L, ], raw_cols_rgb[3L, ],
+    alpha = alpha * 255L, names = names(raw_cols),
+    maxColorValue = 255L
+  )
+  
+  scales::manual_pal(unname(alpha_cols))
+}
+
+
+scale_color_berkeley <- function(palette = c("berkeley"), alpha = 1, ...) {
+  palette <- match.arg(palette)
+  discrete_scale("colour", "berkeley", pal_berkeley(palette, alpha), ...)
+}
+
+scale_fill_berkeley <- function(palette = c("berkeley"), alpha = 1, ...) {
+  palette <- match.arg(palette)
+  discrete_scale("fill", "berkeley", pal_berkeley(palette, alpha), ...)
+}
