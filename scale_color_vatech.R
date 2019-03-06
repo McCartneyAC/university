97311b6a1f44f2a5dd78a0b8f8a5ee344abba01f
Andrew McCartney <acm9q@virginia.edu>
2019-03-05 21:18:35 -0500

    Update scale_color_vatech.R

--- scale_color_vatech.R
+++ scale_color_vatech.R
@@ -11,3 +11,31 @@ Sunset = "#ED8B00",
 turquoise = "#2CD5C4",
 skipper = "#E5E1E6",
 landgrant = "#D7D2CB")
+
+
+pal_vatech <- function(palette = c("vatech"), alpha = 1) {
+  palette <- match.arg(palette)
+  
+  if (alpha > 1L | alpha <= 0L) stop("alpha must be in (0, 1]")
+  
+  raw_cols <- vatech
+  raw_cols_rgb <- col2rgb(raw_cols)
+  alpha_cols <- rgb(
+    raw_cols_rgb[1L, ], raw_cols_rgb[2L, ], raw_cols_rgb[3L, ],
+    alpha = alpha * 255L, names = names(raw_cols),
+    maxColorValue = 255L
+  )
+  
+  scales::manual_pal(unname(alpha_cols))
+}
+
+
+scale_color_vatech <- function(palette = c("vatech"), alpha = 1, ...) {
+  palette <- match.arg(palette)
+  discrete_scale("colour", "vatech", pal_vatech(palette, alpha), ...)
+}
+
+scale_fill_vatech <- function(palette = c("vatech"), alpha = 1, ...) {
+  palette <- match.arg(palette)
+  discrete_scale("fill", "vatech", pal_vatech(palette, alpha), ...)
+}
