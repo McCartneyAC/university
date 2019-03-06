ba4598bec0d102790c493c3e10e6725a2e058b23
Andrew McCartney <acm9q@virginia.edu>
2019-03-05 21:19:19 -0500

    Update scale_color_wm.R

--- scale_color_wm.R
+++ scale_color_wm.R
@@ -13,3 +13,30 @@ griffin  = "#183028",
 wren  = "#00313c",
 spirit  = "#f0b323"
 )
+
+pal_wm <- function(palette = c("wm"), alpha = 1) {
+  palette <- match.arg(palette)
+  
+  if (alpha > 1L | alpha <= 0L) stop("alpha must be in (0, 1]")
+  
+  raw_cols <- wm
+  raw_cols_rgb <- col2rgb(raw_cols)
+  alpha_cols <- rgb(
+    raw_cols_rgb[1L, ], raw_cols_rgb[2L, ], raw_cols_rgb[3L, ],
+    alpha = alpha * 255L, names = names(raw_cols),
+    maxColorValue = 255L
+  )
+  
+  scales::manual_pal(unname(alpha_cols))
+}
+
+
+scale_color_wm <- function(palette = c("wm"), alpha = 1, ...) {
+  palette <- match.arg(palette)
+  discrete_scale("colour", "wm", pal_wm(palette, alpha), ...)
+}
+
+scale_fill_wm <- function(palette = c("wm"), alpha = 1, ...) {
+  palette <- match.arg(palette)
+  discrete_scale("fill", "wm", pal_wm(palette, alpha), ...)
+}
