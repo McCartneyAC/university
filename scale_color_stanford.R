81cac6743bc7745344cf911c9faadf5aa03dcd44
Andrew McCartney <acm9q@virginia.edu>
2019-03-05 21:17:46 -0500

    Update scale_color_stanford.R

--- scale_color_stanford.R
+++ scale_color_stanford.R
@@ -25,3 +25,31 @@ gold = "#b26f16",
 sun  = "#eaab00",
 poppy = "#e98300"
 )
+
+
+pal_stanford <- function(palette = c("stanford"), alpha = 1) {
+  palette <- match.arg(palette)
+  
+  if (alpha > 1L | alpha <= 0L) stop("alpha must be in (0, 1]")
+  
+  raw_cols <- stanford
+  raw_cols_rgb <- col2rgb(raw_cols)
+  alpha_cols <- rgb(
+    raw_cols_rgb[1L, ], raw_cols_rgb[2L, ], raw_cols_rgb[3L, ],
+    alpha = alpha * 255L, names = names(raw_cols),
+    maxColorValue = 255L
+  )
+  
+  scales::manual_pal(unname(alpha_cols))
+}
+
+
+scale_color_stanford <- function(palette = c("stanford"), alpha = 1, ...) {
+  palette <- match.arg(palette)
+  discrete_scale("colour", "stanford", pal_stanford(palette, alpha), ...)
+}
+
+scale_fill_stanford <- function(palette = c("stanford"), alpha = 1, ...) {
+  palette <- match.arg(palette)
+  discrete_scale("fill", "stanford", pal_stanford(palette, alpha), ...)
+}
