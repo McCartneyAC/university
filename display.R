dc1c8c5115edaa534c3cfe26e16ad3bc8f4ab13d
Andrew McCartney <acm9q@virginia.edu>
2019-03-05 21:22:41 -0500

    Update display.R

--- display.R
+++ display.R
@@ -49,3 +49,43 @@ palettes<-list(
     yale = c("#00356b","#286dc0","#63aaff","#222222","#4a4a4a","#978d85","#dddddd","#f9f9f9","#5f712d","#bd5319")
 )
 
+
+###############
+
+palette <- function(name, n, type = c("discrete", "continuous")) {
+  type <- match.arg(type)
+
+  pal <- palettes[[name]]
+  if (is.null(pal))
+    stop("Palette not found.")
+
+  if (missing(n)) {
+    n <- length(pal)
+  }
+
+  if (type == "discrete" && n > length(pal)) {
+    stop("Number of requested colors greater than what palette can offer")
+  }
+
+  out <- switch(type,
+    continuous = grDevices::colorRampPalette(pal)(n),
+    discrete = pal[1:n]
+  )
+  structure(out, class = "palette", name = name)
+}
+
+
+print.palette <- function(x, ...) {
+  n <- length(x)
+  old <- par(mar = c(0.5, 0.5, 0.5, 0.5))
+  on.exit(par(old))
+
+  image(1:n, 1, as.matrix(1:n), col = x,
+    ylab = "", xaxt = "n", yaxt = "n", bty = "n")
+
+  rect(0, 0.9, n + 1, 1.1, col = rgb(1, 1, 1, 0.8), border = NA)
+  text((n + 1) / 2, 1, labels = attr(x, "name"), cex = 1, family = "serif")
+}
+
+#pal <- palette(name = "american", 40, type = "continuous")
+#print.palette(pal)
